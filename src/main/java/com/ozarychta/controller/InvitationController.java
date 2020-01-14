96088db5054bbb6c4034b3a5bc99cea1bbb63598
Oliwia <oliwia.zarychta@gmail.com>
2020-01-14 04:09:10 +0100

    Implemented methods for invitation endpoint

---
+++ src/main/java/com/ozarychta/controller/InvitationController.java
@@ -0,0 +1,32 @@
+package com.ozarychta.controller;
+
+import com.ozarychta.model.Day;
+import com.ozarychta.model.Invitation;
+import com.ozarychta.repository.DayRepository;
+import com.ozarychta.repository.InvitationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+public class InvitationController {
+
+    @Autowired
+    private InvitationRepository invitationRepository;
+
+    @GetMapping("/invitations")
+    public ResponseEntity getInvitations(@RequestHeader("authorization") String authString) {
+        //add authentication
+        return new ResponseEntity(invitationRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/invitations")
+    public @ResponseBody ResponseEntity createDay(@RequestHeader("authorization") String authString,
+                                                  @Valid @RequestBody Invitation invitation) {
+        //authorization and adding user to add
+        return new ResponseEntity(invitationRepository.save(invitation), HttpStatus.OK);
+    }
+}
--- src/main/java/com/ozarychta/enums/InvitationType.java
+++ src/main/java/com/ozarychta/enums/InvitationType.java
@@ -1,6 +1,6 @@
 package com.ozarychta.enums;
 
 public enum InvitationType {
-    CHALLENGE,
-    FRIENDS
+    TO_CHALLENGE,
+    TO_FRIENDS
 }

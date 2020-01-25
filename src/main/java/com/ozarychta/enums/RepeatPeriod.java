e1b14268f0180fdb15f24ab0390d3d677ba9a903
Oliwia <oliwia.zarychta@gmail.com>
2020-01-26 00:00:04 +0100

    Updated enum

--- src/main/java/com/ozarychta/enums/RepeatPeriod.java
+++ src/main/java/com/ozarychta/enums/RepeatPeriod.java
@@ -1,20 +1,11 @@
 package com.ozarychta.enums;
 
 public enum RepeatPeriod {
-    ONE_PER_WEEK,
-    TWO_PER_WEEK,
+    ONCE_PER_WEEK,
+    TWICE_PER_WEEK,
     THREE_PER_WEEK,
     FOUR_PER_WEEK,
     FIVE_PER_WEEK,
     SIX_PER_WEEK,
-    SEVEN_PER_WEEK,
-    ON_MONDAYS,
-    ON_TUESDAYS,
-    ON_WEDNESDAYS,
-    ON_THURSDAYS,
-    ON_FRIDAYS,
-    ON_SATURDAYS,
-    ON_SUNDAYS,
-    ON_WEEKENDS,
-    ON_WORKDAYS
+    EVERYDAY
 }

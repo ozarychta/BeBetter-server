57f4eb4c528504651dd5c76460fbb9ff1e5f8860
ozarychta <oliwia.zarychta@gmail.com>
2020-05-26 03:56:14 +0200

    Added autorization and sorting in CommentController

--- src/main/java/com/ozarychta/controller/CommentController.java
+++ src/main/java/com/ozarychta/controller/CommentController.java
@@ -1,10 +1,13 @@
 package com.ozarychta.controller;
 
+import com.ozarychta.TokenVerifier;
 import com.ozarychta.exception.ResourceNotFoundException;
 import com.ozarychta.model.Comment;
+import com.ozarychta.model.User;
 import com.ozarychta.modelDTO.CommentDTO;
 import com.ozarychta.repository.ChallengeRepository;
 import com.ozarychta.repository.CommentRepository;
+import com.ozarychta.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,19 +25,30 @@ public class CommentController {
     @Autowired
     private ChallengeRepository challengeRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/challenges/{challengeId}/comments")
     public ResponseEntity<List<CommentDTO>> getCommentsByChallengeId(@PathVariable Long challengeId) {
-        return new ResponseEntity(commentRepository.findByChallengeId(challengeId).stream().map(comment -> new CommentDTO(comment)), HttpStatus.OK);
+        return new ResponseEntity(commentRepository.findByChallengeIdOrderByCreatedAtAsc(challengeId)
+                .stream().map(comment -> new CommentDTO(comment)), HttpStatus.OK);
     }
 
     @PostMapping("/challenges/{challengeId}/comments")
     public ResponseEntity addComment(@RequestHeader("authorization") String authString,
                                      @PathVariable Long challengeId,
                               @Valid @RequestBody Comment comment) {
-        //authorization to add
+
+        String googleUserId = TokenVerifier.getInstance().getGoogleUserId(authString).getGoogleUserId();
+
         return new ResponseEntity(challengeRepository.findById(challengeId)
                 .map(challenge -> {
                     comment.setChallenge(challenge);
+
+                    User u = userRepository.findByGoogleUserId(googleUserId).orElseThrow(() -> new ResourceNotFoundException(
+                            "USer with google id " + googleUserId + " not found."));
+                    comment.setCreator(u);
+
                     return commentRepository.save(comment);
                 }).orElseThrow(() -> new ResourceNotFoundException(
                         "Challenge with id " + challengeId + " not found.")), HttpStatus.OK);
--- src/main/java/com/ozarychta/repository/CommentRepository.java
+++ src/main/java/com/ozarychta/repository/CommentRepository.java
@@ -11,4 +11,6 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpecificationExecutor {
 
     List<Comment> findByChallengeId(Long challengeId);
+
+    List<Comment> findByChallengeIdOrderByCreatedAtAsc(Long challengeId);
 }
